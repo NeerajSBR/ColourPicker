0b5129627ffa17f7cadcb98bf004006f9d688f03
NeerajSBR <neerajskanda2001@gmail.com>
2022-07-16 23:14:28 +0530

    second commit

--- app/src/main/java/com/farizma/colorpalettefromimage/MainActivity.java
+++ app/src/main/java/com/farizma/colorpalettefromimage/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 colorPickerpage();
-                finish();
+
             }
         });
 
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showDialog();
-                finish();
+
             }
         });
 
@@ -102,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showAlert();
-                finish();
             }
         });
     }
