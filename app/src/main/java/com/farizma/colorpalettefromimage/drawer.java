7d3055ed46bbf24f2425e320a91b8888661c6fe4
NeerajSBR <neerajskanda2001@gmail.com>
2022-07-27 07:53:18 +0530

    last commit

--- app/src/main/java/com/farizma/colorpalettefromimage/drawer.java
+++ app/src/main/java/com/farizma/colorpalettefromimage/drawer.java
@@ -1,24 +1,33 @@
 package com.farizma.colorpalettefromimage;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 
 import static com.farizma.colorpalettefromimage.display.colorList;
 import static com.farizma.colorpalettefromimage.display.current_brush;
 import static com.farizma.colorpalettefromimage.display.pathList;
 
-public class drawer extends AppCompatActivity {
+public class drawer extends AppCompatActivity implements View.OnClickListener {
+    private Button imgbutton;
     public static Path path = new Path();
     public static Paint paint_brush = new Paint();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+        imgbutton = (Button)findViewById(R.id.save_btn);
+        imgbutton.setOnClickListener(this);
+        imgbutton.setVisibility(View.GONE);
+
     }
 
     public void pencil(View view) {
@@ -54,5 +63,26 @@ public class drawer extends AppCompatActivity {
     public void currentColor(int c) {
         current_brush = c;
         path = new Path();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.save_btn) {
+            AlertDialog.Builder saveDialogue = new AlertDialog.Builder(this);
+            saveDialogue.setTitle("Save?");
+            saveDialogue.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            saveDialogue.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int i) {
+                    dialog.cancel();
+                }
+            });
+            saveDialogue.show();
+        }
     }
 }
